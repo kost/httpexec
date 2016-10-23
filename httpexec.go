be7d1bf303e8531f1a2a84bfc858127f4a5edf0c
Vlatko Kosturjak <kost@linux.hr>
2016-10-23 08:35:26 +0200

    People will have problems building with these ciphers

--- httpexec.go
+++ httpexec.go
@@ -214,8 +214,9 @@ func main() {
 				CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 				PreferServerCipherSuites: true,
 				CipherSuites: []uint16{
-					tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					// turn on for beter security if you have supported
+					// tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+					// tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 					tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
 					tls.TLS_RSA_WITH_AES_256_CBC_SHA,
