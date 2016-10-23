05da629e855e9eab3baa41d874adc67a8e80942f
Davor Kapsa <davor.kapsa@gmail.com>
2016-10-23 17:46:52 +0200

    Use go fmt

--- httpexec.go
+++ httpexec.go
@@ -5,50 +5,54 @@
 package main
 
 import (
-	"flag"
-	"log"
-	"io/ioutil"
+	"bytes"
 	"crypto/tls"
 	"crypto/x509"
-	"net/url"
-	"net/http"
-	"net/http/cgi"
 	"encoding/base64"
 	"encoding/json"
-	"strings"
-	"bytes"
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/cgi"
+	"net/url"
 	"os/exec"
+	"strings"
 )
 
 // JSON input request
-type CmdReq struct{
-	Cmd	string
-	Nojson	bool
-	Stdin	string
+type CmdReq struct {
+	Cmd    string
+	Nojson bool
+	Stdin  string
 }
 
 // JSON output request
-type CmdResp struct{
-	Cmd	string
-	Stdout	string
-	Stderr	string
-	Err	string
+type CmdResp struct {
+	Cmd    string
+	Stdout string
+	Stderr string
+	Err    string
 }
 
-var auth string		// basic authentication combo
-var realm string	// basic authentication realm
-var VerboseLevel int	// global verbosity level
-var SilentOutput bool	// silent output
+var auth string       // basic authentication combo
+var realm string      // basic authentication realm
+var VerboseLevel int  // global verbosity level
+var SilentOutput bool // silent output
 
 // check basic authentication if set
 func checkAuth(w http.ResponseWriter, r *http.Request) bool {
 	s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
-	if len(s) != 2 { return false }
+	if len(s) != 2 {
+		return false
+	}
 
 	b, err := base64.StdEncoding.DecodeString(s[1])
-	if err != nil { return false }
+	if err != nil {
+		return false
+	}
 
-	return bytes.Equal(b,[]byte(auth))
+	return bytes.Equal(b, []byte(auth))
 }
 
 // real content Handler
@@ -57,43 +61,47 @@ func contHandler(w http.ResponseWriter, r *http.Request) {
 	var inputjson CmdReq
 	var outputjson CmdResp
 	var body []byte
-	if (r.Header.Get("Content-Type") == "application/json") {
+	if r.Header.Get("Content-Type") == "application/json" {
 		w.Header().Set("Content-Type", "application/json")
 		jsonout = true
 	} else {
 		w.Header().Set("Content-Type", "text/plain")
 	}
-	cmdstr:=""
-	urlq,_:=url.QueryUnescape(r.URL.RawQuery)
-	if (r.Method == "GET" || r.Method == "HEAD") {
-		cmdstr=urlq
+	cmdstr := ""
+	urlq, _ := url.QueryUnescape(r.URL.RawQuery)
+	if r.Method == "GET" || r.Method == "HEAD" {
+		cmdstr = urlq
 	}
-	if (r.Method == "POST") {
+	if r.Method == "POST" {
 		var rerr error
 		body, rerr = ioutil.ReadAll(r.Body)
 		if rerr != nil {
 		}
-		if (VerboseLevel>2) { log.Printf("Body: %s", body) }
+		if VerboseLevel > 2 {
+			log.Printf("Body: %s", body)
+		}
 
-		if (len(urlq)>0 && r.Method == "POST") {
-			cmdstr=urlq
+		if len(urlq) > 0 && r.Method == "POST" {
+			cmdstr = urlq
 		} else {
-			if (jsonout) {
-				jerr := json.Unmarshal(body,&inputjson)
+			if jsonout {
+				jerr := json.Unmarshal(body, &inputjson)
 				if jerr != nil {
-				    // http.Error(w, jerr.Error(), 400)
-				    return
+					// http.Error(w, jerr.Error(), 400)
+					return
 				}
-				cmdstr=inputjson.Cmd
-				jsonout=!inputjson.Nojson
+				cmdstr = inputjson.Cmd
+				jsonout = !inputjson.Nojson
 			} else {
-				cmdstr=string(body)
+				cmdstr = string(body)
 			}
 		}
 	}
-	if (VerboseLevel>0) { log.Printf("Command to execute: %s", cmdstr) }
+	if VerboseLevel > 0 {
+		log.Printf("Command to execute: %s", cmdstr)
+	}
 
-	if len(cmdstr)<1 {
+	if len(cmdstr) < 1 {
 		return
 	}
 
@@ -104,22 +112,26 @@ func contHandler(w http.ResponseWriter, r *http.Request) {
 	cmd := exec.Command(head, parts...)
 
 	// Handle stdin if have any
-	if (len(urlq)>0 && r.Method == "POST") {
-		if (VerboseLevel>2) { log.Printf("Stdin: %s", body) }
+	if len(urlq) > 0 && r.Method == "POST" {
+		if VerboseLevel > 2 {
+			log.Printf("Stdin: %s", body)
+		}
 		cmd.Stdin = bytes.NewReader(body)
 	}
-	if (len(inputjson.Stdin)>0) {
-		if (VerboseLevel>2) { log.Printf("JSON Stdin: %s", inputjson.Stdin) }
+	if len(inputjson.Stdin) > 0 {
+		if VerboseLevel > 2 {
+			log.Printf("JSON Stdin: %s", inputjson.Stdin)
+		}
 		cmd.Stdin = strings.NewReader(inputjson.Stdin)
 	}
 
 	var err error
 	var jStdout bytes.Buffer
 	var jStderr bytes.Buffer
-	if (r.Method == "HEAD") {
+	if r.Method == "HEAD" {
 		err = cmd.Start()
 	} else {
-		if (jsonout) {
+		if jsonout {
 			cmd.Stdout = &jStdout
 			cmd.Stderr = &jStderr
 		} else {
@@ -129,24 +141,28 @@ func contHandler(w http.ResponseWriter, r *http.Request) {
 		err = cmd.Run()
 	}
 	if err != nil {
-		if (VerboseLevel>0) { log.Printf("Error executing: %s", err) }
-		if (jsonout) {
-			outputjson.Err=err.Error()
+		if VerboseLevel > 0 {
+			log.Printf("Error executing: %s", err)
+		}
+		if jsonout {
+			outputjson.Err = err.Error()
 		} else {
-			if (!SilentOutput) { w.Write([]byte(err.Error())) }
+			if !SilentOutput {
+				w.Write([]byte(err.Error()))
+			}
 		}
 	}
 
-	if (jsonout) {
-		outputjson.Stdout=jStdout.String()
-		outputjson.Stderr=jStderr.String()
-		outputjson.Cmd=cmdstr
+	if jsonout {
+		outputjson.Stdout = jStdout.String()
+		outputjson.Stderr = jStderr.String()
+		outputjson.Cmd = cmdstr
 		json.NewEncoder(w).Encode(outputjson)
 	}
 }
 
-func retlogstr(entry string) (string) {
-	if (len(entry)>0) {
+func retlogstr(entry string) string {
+	if len(entry) > 0 {
 		return entry
 	} else {
 		return "-"
@@ -155,10 +171,10 @@ func retlogstr(entry string) (string) {
 
 // main handler which basically checks (basic) authentication first
 func handler(w http.ResponseWriter, r *http.Request) {
-	if (VerboseLevel>0) {
+	if VerboseLevel > 0 {
 		log.Printf("%s %s %s %s %s", retlogstr(r.RemoteAddr), retlogstr(r.Header.Get("X-Forwarded-For")), r.Method, r.RequestURI, retlogstr(r.URL.RawQuery))
 	}
-	if (auth == "") {
+	if auth == "" {
 		contHandler(w, r)
 	} else {
 		if checkAuth(w, r) {
@@ -180,8 +196,8 @@ func main() {
 	uri := flag.String("uri", "/", "URI to serve")
 	listen := flag.String("listen", ":8080", "listen address and port")
 	flag.StringVar(&realm, "realm", "httpexec", "Basic authentication realm")
-	opttls := flag.Bool("tls",false,"use TLS/SSL")
-	optssl := flag.Bool("ssl",false,"use TLS/SSL")
+	opttls := flag.Bool("tls", false, "use TLS/SSL")
+	optssl := flag.Bool("ssl", false, "use TLS/SSL")
 	optverify := flag.String("verify", "", "Client cert verification using SSL/TLS (CA) certificate file")
 	flag.BoolVar(&SilentOutput, "silentout", false, "Silent Output (do not display errors)")
 	flag.IntVar(&VerboseLevel, "verbose", 0, "verbose level")
@@ -189,29 +205,35 @@ func main() {
 	flag.Parse()
 
 	// turn on tls if client verification is specified
-	if len(*optverify)>0 {
-		*opttls=true
+	if len(*optverify) > 0 {
+		*opttls = true
 	}
 
-	httpProto:="http"
-	if (*opttls || *optssl) {
-		httpProto="https"
+	httpProto := "http"
+	if *opttls || *optssl {
+		httpProto = "https"
 	}
 
-	if (VerboseLevel>0) { log.Printf("Starting to listen at %s with URI %s as %s", *listen, *uri, httpProto) }
-	if (VerboseLevel>5 && len(auth)>0) { log.Printf("Using basic authentication: %s", auth) }
-	if (VerboseLevel>1 && len(*optverify)>0) { log.Printf("Using TLS/SSL client verification with: %s", *optverify) }
+	if VerboseLevel > 0 {
+		log.Printf("Starting to listen at %s with URI %s as %s", *listen, *uri, httpProto)
+	}
+	if VerboseLevel > 5 && len(auth) > 0 {
+		log.Printf("Using basic authentication: %s", auth)
+	}
+	if VerboseLevel > 1 && len(*optverify) > 0 {
+		log.Printf("Using TLS/SSL client verification with: %s", *optverify)
+	}
 
-	if (*optcgi) {
+	if *optcgi {
 		cgi.Serve(http.HandlerFunc(handler))
 	} else {
 		http.HandleFunc(*uri, handler)
 		var err error
-		if (*opttls || *optssl) {
+		if *opttls || *optssl {
 			// secure default TLS configuration
 			tlsCfg := &tls.Config{
-				MinVersion:	tls.VersionTLS12,
-				CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+				MinVersion:               tls.VersionTLS12,
+				CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 				PreferServerCipherSuites: true,
 				CipherSuites: []uint16{
 					// turn on for beter security if you have supported
@@ -223,7 +245,7 @@ func main() {
 				},
 			}
 			// if verify is specified add only specific (CA) certificate to cert pool
-			if (len(*optverify)>0) {
+			if len(*optverify) > 0 {
 				caCertPool := x509.NewCertPool()
 				caCert, err := ioutil.ReadFile(*optverify)
 				if err != nil {
@@ -237,8 +259,8 @@ func main() {
 			}
 
 			srv := &http.Server{
-				Addr:		*listen,
-				TLSConfig:	tlsCfg,
+				Addr:      *listen,
+				TLSConfig: tlsCfg,
 			}
 			err = srv.ListenAndServeTLS(*cert, *key)
 		} else {
@@ -249,4 +271,3 @@ func main() {
 		}
 	}
 }
-
