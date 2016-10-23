fdb60ce4267759525d8640548ea9ce1a5af77532
Vlatko Kosturjak <kost@linux.hr>
2016-10-23 06:22:03 +0200

    Implement secure TLS options by default

--- httpexec.go
+++ httpexec.go
@@ -8,6 +8,7 @@ import (
 	"flag"
 	"log"
 	"io/ioutil"
+	"crypto/tls"
 	"net/url"
 	"net/http"
 	"net/http/cgi"
@@ -193,7 +194,23 @@ func main() {
 		http.HandleFunc(*uri, handler)
 		var err error
 		if (*tls || *ssl) {
-			err = http.ListenAndServeTLS(*listen, *cert, *key, nil)
+			// secure default TLS configuration
+			tlsCfg := &tls.Config{
+				MinVersion:	tls.VersionTLS12,
+				CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+				PreferServerCipherSuites: true,
+				CipherSuites: []uint16{
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+					tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+				},
+			}
+			srv := &http.Server{
+				Addr:		*listen,
+				TLSConfig:	tlsCfg,
+			}
+			err = srv.ListenAndServeTLS(*cert, *key)
 		} else {
 			err = http.ListenAndServe(*listen, nil)
 		}
