39c479d068dab47c5351194811702cdb51842a92
kost <vlatko.kosturjak@gmail.com>
2017-11-06 22:31:52 +0100

    Gometalinter (#3)
    * Use go fmt
    
    * Omit second par
    
    * Add comments for exported vars
    
    * Simplify retlogstr
    
    * Clean shadow declaration
    
    * Close r.Body and log err if exists
    
    * Log url query unescape error
    
    * Check body close err
    
    * Check write error
    
    * Check encode error
    
    * Check 401 write error
    
    * Check cgiserve error
    
    * travis: add 1.6 and 1.7 version

--- httpexec.go
+++ httpexec.go
@@ -5,50 +5,58 @@
 package main
 
 import (
-	"flag"
-	"log"
-	"io/ioutil"
+	"bytes"
 	"crypto/tls"
 	"crypto/x509"
-	"net/url"
-	"net/http"
-	"net/http/cgi"
 	"encoding/base64"
 	"encoding/json"
-	"strings"
-	"bytes"
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/cgi"
+	"net/url"
 	"os/exec"
+	"strings"
 )
 
-// JSON input request
-type CmdReq struct{
-	Cmd	string
-	Nojson	bool
-	Stdin	string
+// CmdReq holds JSON input request.
+type CmdReq struct {
+	Cmd    string
+	Nojson bool
+	Stdin  string
 }
 
-// JSON output request
-type CmdResp struct{
-	Cmd	string
-	Stdout	string
-	Stderr	string
-	Err	string
+// CmdResp holds JSON output request.
+type CmdResp struct {
+	Cmd    string
+	Stdout string
+	Stderr string
+	Err    string
 }
 
-var auth string		// basic authentication combo
-var realm string	// basic authentication realm
-var VerboseLevel int	// global verbosity level
-var SilentOutput bool	// silent output
+var auth string  // basic authentication combo
+var realm string // basic authentication realm
+
+// VerboseLevel holds global verbosity level.
+var VerboseLevel int
+
+// SilentOutput is silent output.
+var SilentOutput bool
 
 // check basic authentication if set
 func checkAuth(w http.ResponseWriter, r *http.Request) bool {
 	s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
-	if len(s) != 2 { return false }
+	if len(s) != 2 {
+		return false
+	}
 
 	b, err := base64.StdEncoding.DecodeString(s[1])
-	if err != nil { return false }
+	if err != nil {
+		return false
+	}
 
-	return bytes.Equal(b,[]byte(auth))
+	return bytes.Equal(b, []byte(auth))
 }
 
 // real content Handler
@@ -57,69 +65,85 @@ func contHandler(w http.ResponseWriter, r *http.Request) {
 	var inputjson CmdReq
 	var outputjson CmdResp
 	var body []byte
-	if (r.Header.Get("Content-Type") == "application/json") {
+	if r.Header.Get("Content-Type") == "application/json" {
 		w.Header().Set("Content-Type", "application/json")
 		jsonout = true
 	} else {
 		w.Header().Set("Content-Type", "text/plain")
 	}
-	cmdstr:=""
-	urlq,_:=url.QueryUnescape(r.URL.RawQuery)
-	if (r.Method == "GET" || r.Method == "HEAD") {
-		cmdstr=urlq
+	cmdstr := ""
+	urlq, urlErr := url.QueryUnescape(r.URL.RawQuery)
+	if urlErr != nil {
+		log.Printf("url query unescape: %v", urlErr)
 	}
-	if (r.Method == "POST") {
+	if r.Method == "GET" || r.Method == "HEAD" {
+		cmdstr = urlq
+	}
+	if r.Method == "POST" {
 		var rerr error
 		body, rerr = ioutil.ReadAll(r.Body)
 		if rerr != nil {
+			log.Printf("read Body: %v", rerr)
 		}
-		if (VerboseLevel>2) { log.Printf("Body: %s", body) }
+		if closeErr := r.Body.Close(); closeErr != nil {
+			log.Printf("body close: %v", closeErr)
 
-		if (len(urlq)>0 && r.Method == "POST") {
-			cmdstr=urlq
+		}
+		if VerboseLevel > 2 {
+			log.Printf("Body: %s", body)
+		}
+
+		if len(urlq) > 0 {
+			cmdstr = urlq
 		} else {
-			if (jsonout) {
-				jerr := json.Unmarshal(body,&inputjson)
+			if jsonout {
+				jerr := json.Unmarshal(body, &inputjson)
 				if jerr != nil {
-				    // http.Error(w, jerr.Error(), 400)
-				    return
+					// http.Error(w, jerr.Error(), 400)
+					return
 				}
-				cmdstr=inputjson.Cmd
-				jsonout=!inputjson.Nojson
+				cmdstr = inputjson.Cmd
+				jsonout = !inputjson.Nojson
 			} else {
-				cmdstr=string(body)
+				cmdstr = string(body)
 			}
 		}
 	}
-	if (VerboseLevel>0) { log.Printf("Command to execute: %s", cmdstr) }
+	if VerboseLevel > 0 {
+		log.Printf("Command to execute: %s", cmdstr)
+	}
 
-	if len(cmdstr)<1 {
+	if len(cmdstr) < 1 {
 		return
 	}
 
 	parts := strings.Fields(cmdstr)
 	head := parts[0]
-	parts = parts[1:len(parts)]
+	parts = parts[1:]
 
 	cmd := exec.Command(head, parts...)
 
 	// Handle stdin if have any
-	if (len(urlq)>0 && r.Method == "POST") {
-		if (VerboseLevel>2) { log.Printf("Stdin: %s", body) }
+	if len(urlq) > 0 && r.Method == "POST" {
+		if VerboseLevel > 2 {
+			log.Printf("Stdin: %s", body)
+		}
 		cmd.Stdin = bytes.NewReader(body)
 	}
-	if (len(inputjson.Stdin)>0) {
-		if (VerboseLevel>2) { log.Printf("JSON Stdin: %s", inputjson.Stdin) }
+	if len(inputjson.Stdin) > 0 {
+		if VerboseLevel > 2 {
+			log.Printf("JSON Stdin: %s", inputjson.Stdin)
+		}
 		cmd.Stdin = strings.NewReader(inputjson.Stdin)
 	}
 
 	var err error
 	var jStdout bytes.Buffer
 	var jStderr bytes.Buffer
-	if (r.Method == "HEAD") {
+	if r.Method == "HEAD" {
 		err = cmd.Start()
 	} else {
-		if (jsonout) {
+		if jsonout {
 			cmd.Stdout = &jStdout
 			cmd.Stderr = &jStderr
 		} else {
@@ -129,36 +153,44 @@ func contHandler(w http.ResponseWriter, r *http.Request) {
 		err = cmd.Run()
 	}
 	if err != nil {
-		if (VerboseLevel>0) { log.Printf("Error executing: %s", err) }
-		if (jsonout) {
-			outputjson.Err=err.Error()
+		if VerboseLevel > 0 {
+			log.Printf("Error executing: %s", err)
+		}
+		if jsonout {
+			outputjson.Err = err.Error()
 		} else {
-			if (!SilentOutput) { w.Write([]byte(err.Error())) }
+			if !SilentOutput {
+				_, writeErr := w.Write([]byte(err.Error()))
+				if writeErr != nil {
+					log.Printf("write: %v", writeErr)
+				}
+			}
 		}
 	}
 
-	if (jsonout) {
-		outputjson.Stdout=jStdout.String()
-		outputjson.Stderr=jStderr.String()
-		outputjson.Cmd=cmdstr
-		json.NewEncoder(w).Encode(outputjson)
+	if jsonout {
+		outputjson.Stdout = jStdout.String()
+		outputjson.Stderr = jStderr.String()
+		outputjson.Cmd = cmdstr
+		if encodeErr := json.NewEncoder(w).Encode(outputjson); encodeErr != nil {
+			log.Printf("encode: %v", err)
+		}
 	}
 }
 
-func retlogstr(entry string) (string) {
-	if (len(entry)>0) {
-		return entry
-	} else {
+func retlogstr(entry string) string {
+	if len(entry) == 0 {
 		return "-"
 	}
+	return entry
 }
 
 // main handler which basically checks (basic) authentication first
 func handler(w http.ResponseWriter, r *http.Request) {
-	if (VerboseLevel>0) {
+	if VerboseLevel > 0 {
 		log.Printf("%s %s %s %s %s", retlogstr(r.RemoteAddr), retlogstr(r.Header.Get("X-Forwarded-For")), r.Method, r.RequestURI, retlogstr(r.URL.RawQuery))
 	}
-	if (auth == "") {
+	if auth == "" {
 		contHandler(w, r)
 	} else {
 		if checkAuth(w, r) {
@@ -167,7 +199,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		}
 		w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
 		w.WriteHeader(401)
-		w.Write([]byte("401 Unauthorized\n"))
+		_, err := w.Write([]byte("401 Unauthorized\n"))
+		if err != nil {
+			log.Printf("401 write: %v", err)
+		}
 	}
 }
 
@@ -180,8 +215,8 @@ func main() {
 	uri := flag.String("uri", "/", "URI to serve")
 	listen := flag.String("listen", ":8080", "listen address and port")
 	flag.StringVar(&realm, "realm", "httpexec", "Basic authentication realm")
-	opttls := flag.Bool("tls",false,"use TLS/SSL")
-	optssl := flag.Bool("ssl",false,"use TLS/SSL")
+	opttls := flag.Bool("tls", false, "use TLS/SSL")
+	optssl := flag.Bool("ssl", false, "use TLS/SSL")
 	optverify := flag.String("verify", "", "Client cert verification using SSL/TLS (CA) certificate file")
 	flag.BoolVar(&SilentOutput, "silentout", false, "Silent Output (do not display errors)")
 	flag.IntVar(&VerboseLevel, "verbose", 0, "verbose level")
@@ -189,29 +224,38 @@ func main() {
 	flag.Parse()
 
 	// turn on tls if client verification is specified
-	if len(*optverify)>0 {
-		*opttls=true
+	if len(*optverify) > 0 {
+		*opttls = true
 	}
 
-	httpProto:="http"
-	if (*opttls || *optssl) {
-		httpProto="https"
+	httpProto := "http"
+	if *opttls || *optssl {
+		httpProto = "https"
 	}
 
-	if (VerboseLevel>0) { log.Printf("Starting to listen at %s with URI %s as %s", *listen, *uri, httpProto) }
-	if (VerboseLevel>5 && len(auth)>0) { log.Printf("Using basic authentication: %s", auth) }
-	if (VerboseLevel>1 && len(*optverify)>0) { log.Printf("Using TLS/SSL client verification with: %s", *optverify) }
+	if VerboseLevel > 0 {
+		log.Printf("Starting to listen at %s with URI %s as %s", *listen, *uri, httpProto)
+	}
+	if VerboseLevel > 5 && len(auth) > 0 {
+		log.Printf("Using basic authentication: %s", auth)
+	}
+	if VerboseLevel > 1 && len(*optverify) > 0 {
+		log.Printf("Using TLS/SSL client verification with: %s", *optverify)
+	}
 
-	if (*optcgi) {
-		cgi.Serve(http.HandlerFunc(handler))
+	if *optcgi {
+		cgiErr := cgi.Serve(http.HandlerFunc(handler))
+		if cgiErr != nil {
+			log.Printf("cgiErr: %v", cgiErr)
+		}
 	} else {
 		http.HandleFunc(*uri, handler)
 		var err error
-		if (*opttls || *optssl) {
+		if *opttls || *optssl {
 			// secure default TLS configuration
 			tlsCfg := &tls.Config{
-				MinVersion:	tls.VersionTLS12,
-				CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+				MinVersion:               tls.VersionTLS12,
+				CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 				PreferServerCipherSuites: true,
 				CipherSuites: []uint16{
 					// turn on for beter security if you have supported
@@ -223,10 +267,10 @@ func main() {
 				},
 			}
 			// if verify is specified add only specific (CA) certificate to cert pool
-			if (len(*optverify)>0) {
+			if len(*optverify) > 0 {
 				caCertPool := x509.NewCertPool()
-				caCert, err := ioutil.ReadFile(*optverify)
-				if err != nil {
+				caCert, readErr := ioutil.ReadFile(*optverify)
+				if readErr != nil {
 					log.Fatal("Error reading client verification cert: ", err)
 				}
 				caCertPool.AppendCertsFromPEM(caCert)
@@ -237,8 +281,8 @@ func main() {
 			}
 
 			srv := &http.Server{
-				Addr:		*listen,
-				TLSConfig:	tlsCfg,
+				Addr:      *listen,
+				TLSConfig: tlsCfg,
 			}
 			err = srv.ListenAndServeTLS(*cert, *key)
 		} else {
@@ -249,4 +293,3 @@ func main() {
 		}
 	}
 }
-
